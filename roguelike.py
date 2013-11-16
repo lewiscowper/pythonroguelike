e1126d73a1fee4608a1c58151496863bddb629f0
Edward Saxton <eddsax@googlemail.com>
2013-11-16 17:57:06 +0000

    Extract constant data to own file

--- roguelike.py
+++ roguelike.py
@@ -2,58 +2,7 @@ import libtcodpy as libtcod
 import math
 import textwrap
 import shelve
-
-# actual size of the window
-SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
-
-# size of the map
-MAP_WIDTH = 80
-MAP_HEIGHT = 43
-
-# sizes and coordinates relevant for the GUI
-BAR_WIDTH = 20
-PANEL_HEIGHT = 7
-PANEL_Y = SCREEN_HEIGHT - PANEL_HEIGHT
-MSG_X = BAR_WIDTH + 2
-MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
-MSG_HEIGHT = PANEL_HEIGHT - 1
-INVENTORY_WIDTH = 50
-
-# parameters for dungeon generator
-ROOM_MAX_SIZE = 10
-ROOM_MIN_SIZE = 6
-MAX_ROOMS = 30
-
-# spell values
-HEAL_AMOUNT = 40
-MANABUFF_AMOUNT = 40
-LIGHTNING_DAMAGE = 40
-LIGHTNING_RANGE = 5
-CONFUSE_RANGE = 8
-CONFUSE_NUM_TURNS = 10
-FIREBALL_RADIUS = 3
-FIREBALL_DAMAGE = 25
-
-# levelling constants
-LEVEL_UP_BASE = 200
-LEVEL_UP_FACTOR = 150
-LEVEL_SCREEN_WIDTH = 40
-
-CHARACTER_SCREEN_WIDTH = 30
-
-# FOV constants
-FOV_ALGO = 0  # default FOV algorithm
-FOV_LIGHT_WALLS = True  # light walls or not
-TORCH_RADIUS = 10
-
-LIMIT_FPS = 20  # 20 frames-per-second maximum
-
-
-color_dark_wall = libtcod.Color(0, 0, 100)
-color_light_wall = libtcod.Color(130, 110, 50)
-color_dark_ground = libtcod.Color(50, 50, 150)
-color_light_ground = libtcod.Color(200, 180, 50)
+from constants import *
 
 
 class Tile(object):
