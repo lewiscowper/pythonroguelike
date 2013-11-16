aee8cd5cbf840c29c2128e0171c47713475f5680
Edward Saxton <eddsax@googlemail.com>
2013-11-16 18:28:00 +0000

    Extract map helpers to there own file

---
+++ levelmap.py
@@ -0,0 +1,57 @@
+import libtcodpy as libtcod
+from constants import *
+
+
+class Tile(object):
+    # a tile of the map and its properties
+    def __init__(self, blocked, block_sight = None):
+        self.blocked = blocked
+
+        # all tiles start unexplored
+        self.explored = False
+
+        # by default, if a tile is blocked, it also blocks sight
+        if block_sight is None:
+            block_sight = blocked
+        self.block_sight = block_sight
+
+
+class Rect(object):
+    # a rectangle on the map. used to characterize a room.
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        # returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
+
+def create_room(level_map, room):
+    # go through the tiles in the rectangle and make them passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            level_map[x][y].blocked = False
+            level_map[x][y].block_sight = False
+
+
+def create_h_tunnel(level_map, x1, x2, y):
+    # horizontal tunnel. min() and max() are used in case x1>x2
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        level_map[x][y].blocked = False
+        level_map[x][y].block_sight = False
+
+
+def create_v_tunnel(level_map, y1, y2, x):
+    # vertical tunnel
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        level_map[x][y].blocked = False
+        level_map[x][y].block_sight = False
